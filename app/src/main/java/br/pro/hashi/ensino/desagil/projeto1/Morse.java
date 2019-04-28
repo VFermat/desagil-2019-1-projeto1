ede6dea3b6b65fd9d632b59cbc710de12dd53eb6
pedromtelho <pedropauloficial@hotmail.com>
2019-04-28 15:22:37 -0300

    SMS

--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/Morse.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/Morse.java
@@ -3,18 +3,62 @@ package br.pro.hashi.ensino.desagil.projeto1;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.PhoneNumberUtils;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class Morse extends AppCompatActivity {
-    private Button button3;
+    // Método de conveniência para mostrar uma bolha de texto.
+    private void showToast(String text) {
+
+        // Constrói uma bolha de duração curta.
+        Toast toast = Toast.makeText(this, text, Toast.LENGTH_SHORT);
+
+        // Mostra essa bolha.
+        toast.show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_morse);
 
+
+        EditText textMessage = findViewById(R.id.text_message);
+        EditText textPhone = findViewById(R.id.text_phone);
+        Button buttonSend = findViewById(R.id.button_send);
+
+        buttonSend.setOnClickListener((view) -> {
+            String message = textMessage.getText().toString();
+
+            if (message.isEmpty()) {
+                showToast("Mensagem inválida!");
+                return;
+            }
+
+            String phone = textPhone.getText().toString();
+
+            // Esta verificação do número de telefone é bem
+            // rígida, pois exige até mesmo o código do país.
+            if (!PhoneNumberUtils.isGlobalPhoneNumber(phone)) {
+                showToast("Número inválido!");
+                return;
+            }
+
+            // Enviar uma mensagem de SMS. Por simplicidade,
+            // não estou verificando se foi mesmo enviada,
+            // mas é possível fazer uma versão que verifica.
+            SmsManager manager = SmsManager.getDefault();
+            manager.sendTextMessage(phone, null, message, null, null);
+
+            // Limpar o campo para nenhum engraçadinho
+            // ficar apertando o botão várias vezes.
+            textPhone.setText("");
+        });
+/*
         Intent intent = getIntent();
         String message = intent.getStringExtra(DefaultMessages.EXTRA_MESSAGE);
 
@@ -31,7 +75,8 @@ public class Morse extends AppCompatActivity {
                 // Muda de tela.
                 startActivity(new Intent(Morse.this, AddContacts.class));
             }
-        });
+        });*/
     }
+
 
 }

dbc8223e30e49c7a575aae86f835378457b184f3
VFermat <vitor.eller30@gmail.com>
2019-05-07 20:50:27 -0300

    Implemented interface on Morse class and inspected

--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/Morse.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/Morse.java
@@ -23,10 +23,10 @@ import android.widget.Toast;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class Morse extends AppCompatActivity {
+public class Morse extends AppCompatActivity implements ActivityConstants {
 
     private final Translator translator = new Translator();
-    private String message = "";
+    private String message;
     private String morseWord = "";
     private boolean holdMode = false;
     private boolean spaceTimerRunning = false;
@@ -35,10 +35,11 @@ public class Morse extends AppCompatActivity {
     private boolean newLetterRunning = false;
     private EditText editTextMessage;
     private EditText editTextMorseMessage;
+    private Class nextActivity;
+    private String phoneNumber;
+    private String contactName;
 
     @SuppressLint("ClickableViewAccessibility")
-    // Declarando os botões:
-    private EditText textPhone;
 
     // Criando o objeto que contém a base de dados do Firebase.
     private final FirebaseDatabase database = FirebaseDatabase.getInstance();
@@ -55,7 +56,29 @@ public class Morse extends AppCompatActivity {
 
         // Pegando a mensagem/contato selecionado da última tela.
         Intent intent = getIntent();
-        this.message += intent.getStringExtra(DefaultMessages.EXTRA_MESSAGE);
+        int callingActivity = intent.getIntExtra("callingActivity", 0);
+        switch (callingActivity) {
+            case MAINACITVITY:
+                message = "";
+                nextActivity = Contacts.class;
+                break;
+            case DEFAULTMESSAGESACTIVITY:
+                message = intent.getStringExtra("message");
+                nextActivity = Contacts.class;
+                break;
+            case MORSEACTIVITY:
+                throw new NullPointerException();
+            case CONTACTACTIVITY:
+                message = "";
+                phoneNumber = intent.getStringExtra("phoneNumber");
+                contactName = intent.getStringExtra("contactName");
+                nextActivity = null;
+                break;
+            case ADDCONTACTACTIVITY:
+                break;
+            default:
+                throw new NullPointerException();
+        }
 
         this.editTextMessage = findViewById(R.id.text_message);
         updateText();
@@ -72,65 +95,71 @@ public class Morse extends AppCompatActivity {
         Button buttonSendMsg = findViewById(R.id.button_send);
         Button buttonAddMsgToList = findViewById(R.id.button_add);
 
-        // Colocando a mensagem recebida na caixa de texto.
+        // Cria um listener para quando esse botão é apertado.
+        buttonSendMsg.setOnClickListener(sendMessageListener);
 
         // Cria um listener para quando esse botão é apertado.
-        buttonSendMsg.setOnClickListener((view) -> {
-            // Verifica se o aplicativo tem a permissão desejada.
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED) {
-                String message = this.editTextMessage.getText().toString();
-
-                if (message.isEmpty()) {
-                    showToast("Mensagem inválida!");
-                    return;
-                }
-
-                String phone = "5511987448483";
-
-                // Esta verificação do número de telefone é bem
-                // rígida, pois exige até mesmo o código do país.
-                if (!PhoneNumberUtils.isGlobalPhoneNumber(phone)) {
-                    showToast("Número inválido!");
-                    return;
-                }
-
-                // Enviar uma mensagem de SMS. Por simplicidade,
-                // não estou verificando se foi mesmo enviada,
-                // mas é possível fazer uma versão que verifica.
-                SmsManager manager = SmsManager.getDefault();
-                manager.sendTextMessage(phone, null, message, null, null);
-
-                // Limpar o campo para nenhum engraçadinho
-                // ficar apertando o botão várias vezes.
-                this.textPhone.setText("");
-
-            } else {
-
-                // Senão, precisamos pedir essa permissão.
-
-                // Cria um vetor de permissões a pedir. Como queremos
-                // uma só, parece um pouco feio, mas é bem conveniente
-                // quando queremos pedir várias permissões de uma vez.
-                String[] permissions = new String[]{
-                        Manifest.permission.SEND_SMS,
-                };
-
-                ActivityCompat.requestPermissions(this, permissions, REQUEST_SEND_SMS);
-            }
-        });
-
-        // Cria um listener para quando esse botão é apertado.
-        buttonAddMsgToList.setOnClickListener((view) -> {
-            // Pegando o que o usuário escreveu:
-            String message = this.editTextMessage.getText().toString();
-
-            // Adicionando o contato à lista de contatos.
-            this.databaseRoot.child("messages").child(message).setValue(message);
-
-            // Muda para a tela de contatos após o contato ser adicionado.
-            startActivity(new Intent(Morse.this, DefaultMessages.class));
-        });
+        buttonAddMsgToList.setOnClickListener(addMsgToListListener);
     }
+
+    private final View.OnClickListener addMsgToListListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            databaseRoot.child("messages").child(message).setValue(message);
+            startActivity(DefaultMessages.class);
+        }
+    };
+
+    private final View.OnClickListener sendMessageListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (nextActivity == null) {
+                if (ContextCompat.checkSelfPermission(Morse.this, Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED) {
+
+                    if (message.isEmpty()) {
+                        showToast("Mensagem inválida!");
+                        return;
+                    }
+
+                    String phone = "5511987448483";
+
+                    // Esta verificação do número de telefone é bem
+                    // rígida, pois exige até mesmo o código do país.
+                    if (!PhoneNumberUtils.isGlobalPhoneNumber(phoneNumber)) {
+                        showToast("Número inválido!");
+                        return;
+                    }
+
+                    // Enviar uma mensagem de SMS. Por simplicidade,
+                    // não estou verificando se foi mesmo enviada,
+                    // mas é possível fazer uma versão que verifica.
+                    SmsManager manager = SmsManager.getDefault();
+                    manager.sendTextMessage(phone, null, message, null, null);
+
+                    // Limpar o campo para nenhum engraçadinho
+                    // ficar apertando o botão várias vezes.
+                    message = "";
+                    updateText();
+                    startActivity(MainActivity.class);
+
+                } else {
+
+                    // Senão, precisamos pedir essa permissão.
+
+                    // Cria um vetor de permissões a pedir. Como queremos
+                    // uma só, parece um pouco feio, mas é bem conveniente
+                    // quando queremos pedir várias permissões de uma vez.
+                    String[] permissions = new String[]{
+                            Manifest.permission.SEND_SMS,
+                    };
+
+                    ActivityCompat.requestPermissions(Morse.this, permissions, REQUEST_SEND_SMS);
+                }
+            } else {
+                startActivity(nextActivity, message);
+            }
+        }
+    };
 
     private final View.OnClickListener clickListener = new View.OnClickListener() {
         @Override
@@ -225,7 +254,6 @@ public class Morse extends AppCompatActivity {
         }
     };
 
-
     private final CountDownTimer longHoldTimer = new CountDownTimer(3000, 1000) {
         @Override
         public void onTick(long millisUntilFinished) {
@@ -284,5 +312,18 @@ public class Morse extends AppCompatActivity {
 
         // Mostra essa bolha.
         toast.show();
+    }
+
+    private void startActivity(Class c) {
+        Intent intent = new Intent(Morse.this, c);
+        intent.putExtra("callingActivity", MORSEACTIVITY);
+        startActivity(intent);
+    }
+
+    private void startActivity(Class c, String message) {
+        Intent intent = new Intent(Morse.this, c);
+        intent.putExtra("callingActivity", MORSEACTIVITY);
+        intent.putExtra("message", message);
+        startActivity(intent);
     }
 }
